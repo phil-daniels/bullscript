64f85447a90d253bd7c7b648b8eb3168878eba6a
Philip Hignight <philip.hignight@gmail.com>
2022-10-27 11:58:53 -0400

    more vue testing

--- vue_example_1/app.js
+++ vue_example_1/app.js
@@ -1,3 +1,19 @@
 const app = Vue.createApp({
-    
+  template: `
+    <h1>todos</h1>
+    <input v-model="newTodoLabel"/>
+    {{newTodoLabel}}
+    <button v-on:click='increment()'>Up!</button>
+  `,
+  data() {
+    return {
+      newTodoLabel: "",
+    };
+  },
+  methods: {
+    increment() {
+        this.count++;
+    }
+  }
 });
+app.mount("#app");
