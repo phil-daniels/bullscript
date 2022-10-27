bbe7ca553d4f59dd03394fc3181014d36315335a
Phil Daniels <philip.hignight@gmail.com>
2022-10-27 12:26:36 -0400

    more vue testing

--- vue_example_1/app.js
+++ vue_example_1/app.js
@@ -1,19 +1,28 @@
 const app = Vue.createApp({
   template: `
     <h1>todos</h1>
-    <input v-model="newTodoLabel"/>
-    {{newTodoLabel}}
+    <input v-model="newTodoLabel" @keyup.enter="add"/>
+    <template v-for="todo in todos">
+      <div>
+        <div @click="toggleCompleted(todo)">{{todo.label}}</div>
+      </div>
+    </template>
     <button v-on:click='increment()'>Up!</button>
   `,
   data() {
     return {
       newTodoLabel: "",
+      todos: [],
     };
   },
   methods: {
-    increment() {
-        this.count++;
-    }
+    add() {
+      this.todos.push({label: this.newTodoLabel, completed: false});
+      this.newTodoLabel = "";
+    },
+    toggleCompleted(todo) {
+      todo.completed = !todo.completed;
+    },
   }
 });
 app.mount("#app");
